ea880967f47bedcfe676188a6d2cd6cca47bfea9
Jubaw <doguztrk@gmail.com>
2024-07-01 13:25:37 +0300

    added book as object in getLoans method,finished up the method.

--- src/main/java/com/doztrk/libraryproject/payload/mappers/LoanMapper.java
+++ src/main/java/com/doztrk/libraryproject/payload/mappers/LoanMapper.java
@@ -1,13 +1,29 @@
 package com.doztrk.libraryproject.payload.mappers;
 
 
+import com.doztrk.libraryproject.entity.concretes.business.Book;
 import com.doztrk.libraryproject.entity.concretes.business.Loan;
+import com.doztrk.libraryproject.payload.response.business.BookResponse;
 import com.doztrk.libraryproject.payload.response.business.LoanResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class LoanMapper {
 
+    private final BookMapper bookMapper;
+
+    public LoanResponse mapLoanToLoanResponseWithBook(Loan loan){
+        return LoanResponse.builder()
+                .userId(loan.getUser().getId())
+                .bookId(loan.getBook().getId())
+                .loanDate(loan.getLoanDate())
+                .expireDate(loan.getExpireDate())
+                .returnDate(loan.getReturnDate())
+                .book(bookMapper.mapBookToBookResponse(loan.getBook())) // returns book object with it.
+                .build();
+    }
 
     public LoanResponse mapLoanToLoanResponse(Loan loan){
         return LoanResponse.builder()
@@ -18,4 +34,6 @@ public class LoanMapper {
                 .returnDate(loan.getReturnDate())
                 .build();
     }
+
+
 }
